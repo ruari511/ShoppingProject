26b3ed4d2fda695e722562a73d6ff3047fc45122
itwill <itwill@ITWILLBS06-24>
2019-08-13 20:34:58 +0900

    buylist테이블 insert를 위한 테이블 칼럼 추가

--- ShoppingMall/src/net/buyList/db/BuyListDTO.java
+++ ShoppingMall/src/net/buyList/db/BuyListDTO.java
@@ -1,5 +1,7 @@
 package net.buyList.db;
 
+import java.sql.Timestamp;
+
 public class BuyListDTO {
 	private int buynum;
 	private String id;
@@ -9,7 +11,79 @@ public class BuyListDTO {
 	private String delivery_mtel;
 	private String delivery_address;
 	
+	private String order_name;
+	private String order_mtel;
+	private String order_email;
+	private Timestamp buydate;
+	private int coupon_num;
+	private int point;
+	private int delivery_cost;
+	private String delivery_result;
+	private String delivery_message;
+	private String payments;
 	
+	
+	
+	public String getOrder_name() {
+		return order_name;
+	}
+	public void setOrder_name(String order_name) {
+		this.order_name = order_name;
+	}
+	public String getOrder_mtel() {
+		return order_mtel;
+	}
+	public void setOrder_mtel(String order_mtel) {
+		this.order_mtel = order_mtel;
+	}
+	public String getOrder_email() {
+		return order_email;
+	}
+	public void setOrder_email(String order_email) {
+		this.order_email = order_email;
+	}
+	public Timestamp getBuydate() {
+		return buydate;
+	}
+	public void setBuydate(Timestamp buydate) {
+		this.buydate = buydate;
+	}
+	public int getCoupon_num() {
+		return coupon_num;
+	}
+	public void setCoupon_num(int coupon_num) {
+		this.coupon_num = coupon_num;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public int getDelivery_cost() {
+		return delivery_cost;
+	}
+	public void setDelivery_cost(int delivery_cost) {
+		this.delivery_cost = delivery_cost;
+	}
+	public String getDelivery_result() {
+		return delivery_result;
+	}
+	public void setDelivery_result(String delivery_result) {
+		this.delivery_result = delivery_result;
+	}
+	public String getDelivery_message() {
+		return delivery_message;
+	}
+	public void setDelivery_message(String delivery_message) {
+		this.delivery_message = delivery_message;
+	}
+	public String getPayments() {
+		return payments;
+	}
+	public void setPayments(String payments) {
+		this.payments = payments;
+	}
 	public int getBuynum() {
 		return buynum;
 	}
