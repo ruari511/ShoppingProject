29ea5f67fb83d36513f4ddde8c591ce23c9e1a4a
wjdgus5625 <wjdgus5625@naver.com>
2019-08-15 01:24:24 +0900

    구매목록 insert를 하는 기능 구현 날짜형식 및 배송비 쿠폰 선택 유무에 따른 배송료 입력방식 추가

--- ShoppingMall/src/net/cart/action/BuyListInsertController.java
+++ ShoppingMall/src/net/cart/action/BuyListInsertController.java
@@ -1,6 +1,7 @@
 package net.cart.action;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import net.buyList.db.BuyListDAO;
+import net.buyList.db.BuyListDTO;
 
 @WebServlet("/BuyListInsertController.do")
 public class BuyListInsertController extends HttpServlet {
@@ -24,6 +28,11 @@ public class BuyListInsertController extends HttpServlet {
 		
 		request.setCharacterEncoding("UTF-8");
 		
+		BuyListDTO bdto = null;
+		BuyListDAO bdao = new BuyListDAO();
+		
+		int lastprice = Integer.parseInt(request.getParameter("lastprice"));
+		
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("id");
 		String ordername = request.getParameter("ordername");
@@ -31,6 +40,9 @@ public class BuyListInsertController extends HttpServlet {
 		String orderemail = request.getParameter("orderemail");
 		String delivery_title = request.getParameter("delivery_title");
 		String delivery_name = request.getParameter("delivery_name");
+		int delivery_cost = Integer.parseInt(request.getParameter("deltotal"));
+		System.out.println("delivery_cost = " + delivery_cost);
+		
 		
 		System.out.println("id = " + id + "\t ordername = " + ordername);
 		System.out.println("orderphone = " + orderphone + "\t orderemail = " + orderemail);
@@ -46,33 +58,88 @@ public class BuyListInsertController extends HttpServlet {
 		//둘 중 하나는 null임 message_1은 직접 메세지 입력하는 input의 값
 		String delivery_message = request.getParameter("delivery_message_2");
 		String delivery_message_1 = request.getParameter("delivery_message_1");
-		if(delivery_message == null){
-			System.out.println("delivery_message_1 = " + delivery_message_1);
-		} else{
+		String del_message;
+		if(delivery_message == "배송 메시지 직접입력"){
+			del_message = delivery_message;
 			System.out.println("delivery_message = " + delivery_message);
+		} else{
+			del_message = delivery_message_1;
+			System.out.println("delivery_message_1 = " + delivery_message_1);
 		}
 		
-		String[] cartnum = request.getParameterValues("cartnum");
-		
-		if(cartnum != null){
-			for(int i=0; i<cartnum.length; i++){
-				System.out.println("i = " + i + "\t cartnum[i] = " + cartnum[i]);
-			}
-		}
 		String allcouponnum = request.getParameter("allcouponnum");
 		String[] coupon_split = allcouponnum.split("-");
 		allcouponnum = coupon_split[0];
 		
+		if(allcouponnum==null){
+			allcouponnum = "0";
+		}
+		
 		System.out.println("couponnum = " + allcouponnum);
 		
 		String delcouponnum = request.getParameter("delcouponnum");
+		System.out.println("delcouponnum1 = " + delcouponnum);
+		if(delcouponnum=="선택안함" || delcouponnum==null || delcouponnum.length()==4){
+			delcouponnum="0";
+			delivery_cost=2500;
+			if(lastprice>=20000){
+				delivery_cost=0;
+			}
+			System.out.println("del이 null");
+		} else{
+			System.out.println("del이 null이 아님");
+			delivery_cost=0;
+		}
 		
-		System.out.println("delcouponnum = " + delcouponnum);
+		
+		System.out.println("delcouponnum2 = " + delcouponnum);
 		
 		String inpoint = request.getParameter("inpoint");
+		
+		if(inpoint==null){
+			inpoint = "0";
+		}
+		
 		System.out.println("inpoint = " + inpoint);
 		String paytype = request.getParameter("paytype");
 		System.out.println("paytype = " + paytype);
+		
+		String[] cartnum = request.getParameterValues("cartnum");
+		
+		int maxbuynum = bdao.maxBuynum();
+		
+		SimpleDateFormat simpl = new SimpleDateFormat("yyyy/MM/dd");
+		String buydate = simpl.format(System.currentTimeMillis());
+		
+		
+		if(cartnum != null){
+			for(int i=0; i<cartnum.length; i++){
+				System.out.println("i = " + i + "\t cartnum[i] = " + cartnum[i]);
+				bdto = new BuyListDTO();
+				
+				bdto.setBuynum(maxbuynum);
+				bdto.setId(id);
+				bdto.setOrder_name(ordername);
+				bdto.setOrder_mtel(orderphone);
+				bdto.setOrder_email(orderemail);
+				bdto.setBuydate(buydate);
+				bdto.setProduct_num(bdao.selectPronum(Integer.parseInt(cartnum[i])));
+				bdto.setProduct_count(bdao.selectProcount(Integer.parseInt(cartnum[i])));
+				bdto.setAll_coupon_num(Integer.parseInt(allcouponnum));
+				bdto.setDelivery_coupon_num(Integer.parseInt(delcouponnum));
+				bdto.setPoint(Integer.parseInt(inpoint));
+				bdto.setDelivery_title(delivery_title);
+				bdto.setDelivery_cost(delivery_cost);
+				bdto.setDelivery_result("배송준비중");
+				bdto.setDelivery_name(delivery_name);
+				bdto.setDelivery_mtel(delivery_phone);
+				bdto.setDelivery_address("배송주소");
+				bdto.setDelivery_message(del_message);
+				bdto.setPayments(paytype);
+				
+				bdao.insertBuyList(bdto);
+			}
+		}
 		
 	}
 	
