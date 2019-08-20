0ec34c22f1dc56ee81b7ea39f9241632d8349cd8
itwill <itwill@ITWILLBS06-24>
2019-08-20 18:03:18 +0900

    총 상품금액, 총 할인금액, 쿠폰의 최대 할인값, 배송료, 구매목록 List메소드 구하는 추가

--- ShoppingMall/src/net/buyList/db/BuyListDAO.java
+++ ShoppingMall/src/net/buyList/db/BuyListDAO.java
@@ -3,10 +3,13 @@ package net.buyList.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Vector;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import net.cart.db.CartDTO;
 
 public class BuyListDAO {
 
@@ -18,6 +21,47 @@ DataSource ds;
 		ds = (DataSource)ctx.lookup("java:comp/env/jdbc/shoppingmall");
 		return ds.getConnection();
 	}
+	
+	public BuyListDTO getBuyList(int buynum){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		BuyListDTO buyDTO = null;
+		
+		try {
+			con = getConnection();
+			
+			sql  = "select distinct * from buylist where buynum=?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, buynum);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				
+				buyDTO = new BuyListDTO();
+				
+				buyDTO.setBuynum(rs.getInt("buynum"));
+				buyDTO.setDelivery_name(rs.getString("delivery_name"));
+				buyDTO.setDelivery_mtel(rs.getString("delivery_mtel"));
+				buyDTO.setDelivery_address(rs.getString("delivery_address"));
+				
+			}//while문 끝
+			
+		} catch (Exception e) {
+			System.out.println("getAllBuyList()메소드 내부에서의 오류 : " + e);
+		} finally{
+			if(pstmt!=null){ try{pstmt.close();} catch(Exception e){e.printStackTrace();}}
+			if(con!=null){ try{con.close();} catch(Exception e){e.printStackTrace();}}
+			if(rs!=null){ try{rs.close();} catch(Exception e){e.printStackTrace();}}
+		}
+	
+		return buyDTO;
+	}//getCartList() 메소드 끝
 	
 	public int maxBuynum(){
 		
@@ -177,6 +221,202 @@ DataSource ds;
 		
 		return count;
 		
+	}
+	
+	public int buytotalprice(int buynum){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		
+		int totalprice=0;
+		
+		try {
+			
+			con = getConnection();
+			
+			sql  = "select sum((p.product_price-p.product_sale_price)*b.product_count) "
+				 + "from product p join buylist b "
+				 + "on p.product_num=b.product_num "
+				 + "where b.buynum=?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, buynum);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()){
+				totalprice = rs.getInt(1);
+			} else{
+				totalprice = 0;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("buytotalprice()메소드 내부의 오류 : " + e);
+		} finally {
+			if(pstmt!=null){ try{pstmt.close();} catch(Exception e){e.printStackTrace();}}
+			if(con!=null){ try{con.close();} catch(Exception e){e.printStackTrace();}}
+			if(rs!=null){ try{rs.close();} catch(Exception e){e.printStackTrace();}}
+		}
+		
+		return totalprice;
+	}
+	
+	public int buytotalsaleprice(int buynum, int limit){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		
+		int totalsaleprice=0;
+		
+		try {
+			
+			con = getConnection();
+			
+			sql  = "select distinct (select sum((p.product_price-p.product_sale_price)*b.product_count) "
+				 + "from product p join buylist b "
+				 + "on p.product_num=b.product_num "
+				 + "where b.buynum=?)/100*("
+				 + "select distinct c.coupon_percent "
+				 + "from coupon c join buylist b "
+				 + "on c.coupon_num=b.all_coupon_num "
+				 + "where b.buynum=?) as sale "
+				 + "from buylist "
+				 + "where buynum=?";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, buynum);
+			pstmt.setInt(2, buynum);
+			pstmt.setInt(3, buynum);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()){
+				if(rs.getInt(1)>limit)
+				totalsaleprice = limit;
+			} else{
+				totalsaleprice = rs.getInt(1);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("buytotalsaleprice()메소드 내부의 오류 : " + e);
+		} finally {
+			if(pstmt!=null){ try{pstmt.close();} catch(Exception e){e.printStackTrace();}}
+			if(con!=null){ try{con.close();} catch(Exception e){e.printStackTrace();}}
+			if(rs!=null){ try{rs.close();} catch(Exception e){e.printStackTrace();}}
+		}
+		
+		return totalsaleprice;
+	}
+	
+	public int coupon_limitmax(int buynum){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		
+		int limit=0;
+		
+		try {
+			con = getConnection();
+			
+			sql = "select distinct c.coupon_limitmax "
+				+ "from coupon c join buylist b "
+				+ "on c.coupon_num=b.all_coupon_num "
+				+ "where b.buynum=?";
+			pstmt = con.prepareStatement(sql);
+				
+			pstmt.setInt(1, buynum);
+			
+			rs = pstmt.executeQuery();
+				
+			if(rs.next()){
+				limit = rs.getInt(1);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("coupon_limitmax()메소드 내부의 오류 : " + e);
+		} finally {
+			if(pstmt!=null){ try{pstmt.close();} catch(Exception e){e.printStackTrace();}}
+			if(con!=null){ try{con.close();} catch(Exception e){e.printStackTrace();}}
+			if(rs!=null){ try{rs.close();} catch(Exception e){e.printStackTrace();}}
+		}
+		
+		return limit;
+	}
+	
+	public int inpoint(int buynum){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		
+		int point=0;
+		
+		try {
+			con = getConnection();
+			
+			sql = "select point from buylist where buynum=?";
+			pstmt = con.prepareStatement(sql);
+				
+			pstmt.setInt(1, buynum);
+			
+			rs = pstmt.executeQuery();
+				
+			if(rs.next()){
+				point = rs.getInt(1);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("inpoint()메소드 내부의 오류 : " + e);
+		} finally {
+			if(pstmt!=null){ try{pstmt.close();} catch(Exception e){e.printStackTrace();}}
+			if(con!=null){ try{con.close();} catch(Exception e){e.printStackTrace();}}
+			if(rs!=null){ try{rs.close();} catch(Exception e){e.printStackTrace();}}
+		}
+		
+		return point;
+	}
+	
+	public int delivery_cost(int buynum){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		
+		int delivery_cost=0;
+		
+		try {
+			con = getConnection();
+			
+			sql = "select delivery_cost from buylist where buynum=?";
+			pstmt = con.prepareStatement(sql);
+				
+			pstmt.setInt(1, buynum);
+			
+			rs = pstmt.executeQuery();
+				
+			if(rs.next()){
+				delivery_cost = rs.getInt(1);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("delivery_cost()메소드 내부의 오류 : " + e);
+		} finally {
+			if(pstmt!=null){ try{pstmt.close();} catch(Exception e){e.printStackTrace();}}
+			if(con!=null){ try{con.close();} catch(Exception e){e.printStackTrace();}}
+			if(rs!=null){ try{rs.close();} catch(Exception e){e.printStackTrace();}}
+		}
+		
+		return delivery_cost;
 	}
 	
 }
