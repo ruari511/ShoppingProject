d91c75601b48bdf1a64665bcd92072aaf6cdf40c
itwill <itwill@ITWILLBS06-24>
2019-08-21 17:56:07 +0900

    ProductDTO 구조변경

--- ShoppingMall/src/net/product/db/ProductDTO.java
+++ ShoppingMall/src/net/product/db/ProductDTO.java
@@ -2,15 +2,30 @@ package net.product.db;
 
 public class ProductDTO {
 	private int product_num;
+	private String category_main;
+	private String category_sub;
 	private String product_name;
+	private int product_count;
 	private int product_price;
-	private String product_contents;
-	private int category_main;
-	private int category_sub;
+	private int product_sale_price;
+	private String img_main;
+	private String img_contents;
+	private String brand;
 	private int price_count;
-	private String img;
-	private int discount;
 	
+	
+	public int getProduct_count() {
+		return product_count;
+	}
+	public void setProduct_count(int product_count) {
+		this.product_count = product_count;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
 	public int getProduct_num() {
 		return product_num;
 	}
@@ -29,22 +44,22 @@ public class ProductDTO {
 	public void setProduct_price(int product_price) {
 		this.product_price = product_price;
 	}
-	public String getProduct_contents() {
-		return product_contents;
+	public String getImg_contents() {
+		return img_contents;
 	}
-	public void setProduct_contents(String product_contents) {
-		this.product_contents = product_contents;
+	public void setImg_contents(String img_contents) {
+		this.img_contents = img_contents;
 	}
-	public int getCategory_main() {
+	public String getCategory_main() {
 		return category_main;
 	}
-	public void setCategory_main(int category_main) {
+	public void setCategory_main(String category_main) {
 		this.category_main = category_main;
 	}
-	public int getCategory_sub() {
+	public String getCategory_sub() {
 		return category_sub;
 	}
-	public void setCategory_sub(int category_sub) {
+	public void setCategory_sub(String category_sub) {
 		this.category_sub = category_sub;
 	}
 	public int getPrice_count() {
@@ -53,17 +68,17 @@ public class ProductDTO {
 	public void setPrice_count(int price_count) {
 		this.price_count = price_count;
 	}
-	public String getImg() {
-		return img;
+	public String getImg_main() {
+		return img_main;
 	}
-	public void setImg(String img) {
-		this.img = img;
+	public void setImg_main(String img_main) {
+		this.img_main = img_main;
 	}
-	public int getDiscount() {
-		return discount;
+	public int getProduct_sale_price() {
+		return product_sale_price;
 	}
-	public void setDiscount(int discount) {
-		this.discount = discount;
+	public void setProduct_sale_price(int product_sale_price) {
+		this.product_sale_price = product_sale_price;
 	}
 	
 	
