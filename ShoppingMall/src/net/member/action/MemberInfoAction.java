91df3a62d8c48686587cd46fc77c02cdc3e3824c
itwill <itwill@ITWILLBS06-24>
2019-09-09 18:21:29 +0900

    주석 공백 제거

--- ShoppingMall/src/net/member/action/MemberInfoAction.java
+++ ShoppingMall/src/net/member/action/MemberInfoAction.java
@@ -14,61 +14,31 @@ import net.action.ActionForward;
  */
 public class MemberInfoAction implements Action
 {
-
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		System.out.println("MemberInfoAction excute()");
 		ActionForward forward = new ActionForward();
 		
-	
-		
 		// 세션이 가지고있는 로그인한 ID 정보를 가져온다
 		HttpSession session = request.getSession();
 		String id = session.getAttribute("id").toString();
-
-		
-
 		
 		// 수정할 회원정보를 가져온다.
 		MemberDAO dao = new MemberDAO();
-		
-		
-		
 		MemberDTO member = dao.getUser(id);
 		
-		System.out.println("main = " + member.getAddress_main());
-		System.out.println("detail = " + member.getAddress_detail());
-		/*member.setId(request.getParameter("id"));
-		member.setPassword(request.getParameter("password"));
-		member.setEmail(request.getParameter("email"));
-		member.setAddress_main(request.getParameter("address_main"));
-		member.setAddress_detail(request.getParameter("address_detail"));*/
 		// ModifyFrom.jsp에 회원정보를 전달하기 위해 request에 MemberBean을 세팅한다.
 		request.setAttribute("memberInfo", member);
 		request.setAttribute("birth_date", member.getBirth_date());
 		request.setAttribute("address_main", member.getAddress_main());
 		request.setAttribute("address_detail", member.getAddress_detail());
 		
-		
-		/*
-		forward=new ActionForward();
-		//페이지 이동 방식 여부 값 false로 저장-> RequestDispatcher  forward() 방식
-		forward.setRedirect(false); //주소값 노출 안됨
-		//이동할 페이지 경로(탈퇴 페이지) 주소값 저장
-		*/
-		
-		
-		
-		
 		forward.setRedirect(false);
-		//forward.setPath("memberInfoModify.do");
 		
 		forward.setPath("./Main.jsp?section=./member/MemberInfoModify.jsp");
 			
 		return forward;
 		}
-
-	
 	}
 
